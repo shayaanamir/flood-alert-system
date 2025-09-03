05c45f3194af719ea36d33bb9ec788d36a2b1dca
shauncode24 <shaunak.karve@somaiya.edu>
2025-09-03 21:04:56 +0530

    Changed Login Page Styling

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,6 +10,7 @@ const LoginPage = () => {
   const [password, setPassword] = useState("");
   const [confirmPass, setConfirmPass] = useState("");
   const [error, setError] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   const navigate = useNavigate();
 
@@ -20,11 +21,19 @@ const LoginPage = () => {
 
   function toggleStatus() {
     setIsLogin(!isLogin);
+    // setError("");
+    // setEmail("");
+    // setPassword("");
+    // setConfirmPass("");
+    // setSubmitted(false);
+  }
+
+  useEffect(() => {
     setError("");
     setEmail("");
     setPassword("");
     setConfirmPass("");
-  }
+  }, [isLogin]);
 
   function validateEmail(mail) {
     return /\S+@\S+\.\S+/.test(mail);
@@ -32,6 +41,7 @@ const LoginPage = () => {
 
   function handleSubmit(e) {
     e.preventDefault();
+    setSubmitted(true);
     setError("");
 
     if (!validateEmail(email)) {
@@ -67,6 +77,21 @@ const LoginPage = () => {
     <>
       <div className="default login-container">
         <form className="default login-div" onSubmit={handleSubmit}>
+          <svg
+            width="56x"
+            height="56px"
+            viewBox="0 0 20 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 23.6667C12.166 23.6667 14.2432 22.8063 15.7747 21.2747C17.3063 19.7432 18.1667 17.6659 18.1667 15.5C18.1667 13.1667 17 10.95 14.6667 9.08333C12.3334 7.21667 10.5834 4.41667 10 1.5C9.41671 4.41667 7.66671 7.21667 5.33337 9.08333C3.00004 10.95 1.83337 13.1667 1.83337 15.5C1.83337 17.6659 2.69379 19.7432 4.22534 21.2747C5.75688 22.8063 7.83411 23.6667 10 23.6667Z"
+              stroke="#2563EB"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
           <div className="default login-container-header">
             <h1>{!isLogin ? "Welcome Back!" : "Create an Account"}</h1>
             <p>
@@ -98,22 +123,26 @@ const LoginPage = () => {
             />
           )}
 
-          {error && <p className="error-message">{error}</p>}
-
+          <div className="default login-sub-footer">
+            <p>Remember Me</p>
+            <p>Forgot Password?</p>
+          </div>
           <div className="default login-button-div">
             <button type="submit" className="default login-button">
               {!isLogin ? "Login" : "Sign Up"}
             </button>
           </div>
-          <div className="default login-footer">
-            <p>
-              {!isLogin ? "Don't have an account?" : "Already have an account?"}
 
-              <button onClick={toggleStatus} className="status-button">
+          {error && submitted && <p className="error-message">{error}</p>}
+
+          <div className="default login-footer">
+            <p className="default">
+              {!isLogin ? "Don't have an account?" : "Already have an account?"}
+              &nbsp;
+              <div onClick={toggleStatus} className="status-button">
                 {!isLogin ? "Sign Up" : "Log in"}
-              </button>
+              </div>
             </p>
-            <p>Remember Me</p>
           </div>
         </form>
       </div>
