488777dee6a8568da00f8b64e779d7d29661662e
Shayaan Amir <67624423+PlatinumBeing@users.noreply.github.com>
2025-10-28 21:19:00 +0530

    Added MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ import floodRoutes from "./routes/floodRoutes.js";
 import coordinatesRoutes from "./routes/coordinatesRoutes.js";
 import loginRoutes from "./routes/loginRoutes.js";
 import shelterRoutes from "./routes/shelterRoutes.js";
-
+import {MongoClient, ServerApiVersion} from "mongodb";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 // Test route
 app.get("/", (req, res) => {
@@ -25,3 +26,29 @@ app.use("/shelter", shelterRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const uri = "mongodb+srv://floodalert:admin123@floodalert.26hahle.mongodb.net/?appName=FloodAlert";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server    (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
