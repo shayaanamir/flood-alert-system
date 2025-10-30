5bf1774eca65a29227a00e6b4bee41960a191ad5
shauncode24 <shaunak.karve@somaiya.edu>
2025-10-30 12:24:16 +0530

    Added env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,25 @@
-import {MongoClient, ServerApiVersion} from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 
-const uri = "mongodb+srv://floodalert:admin123@floodalert.26hahle.mongodb.net/?appName=FloodAlert";
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function connectDB() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     await client.close();
   }
