8a8338a7663b57123375d7d8eaba76f79b95bbb0
shauncode24 <shaunak.karve@somaiya.edu>
2025-11-05 15:14:34 +0530

    Added fields to signup page (except location)

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence";
+
+const connection = mongoose.connection; // use same connection
+const AutoIncrement = AutoIncrementFactory(connection);
 
 const userSchema = new mongoose.Schema({
-  user_id: { type: String, required: true, unique: true },
+  user_id: { type: Number, unique: true }, // auto-incremented
   full_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String },
+  phone: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
   role: { type: String, default: "user" },
@@ -15,5 +19,7 @@ userSchema.pre("save", function (next) {
   this.updated_at = new Date();
   next();
 });
+
+userSchema.plugin(AutoIncrement, { inc_field: "user_id" });
 
 export default mongoose.model("User", userSchema);
