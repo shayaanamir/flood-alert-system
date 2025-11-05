c6c055d6f0a5cfbcce0ad4b98a91f18907b63a4b
shail-712 <shailshajim@gmail.com>
2025-11-05 14:50:38 +0530

    db error handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
